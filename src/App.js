d7556a522e15681cb7c23878321feb6eee11afb2
kjmin622 <kjmin622@naver.com>
2022-06-25 06:24:03 +0000

    start react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+	<div className = "App">
+	  	hihi
+	</div>
+  );
+}
+
+export default App;
